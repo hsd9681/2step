c4be4d01e101f7a258f258af69365953aedf39c3
gminnimk <tvt1245@naver.com>
2024-07-11 17:50:26 +0900

    update: ColumnService 클래스에서 현재 최대 순서 값을 조회한 후 1을 더하는 방법을 사용하기 위해 메소드 추가

--- src/main/java/com/example/demo/column/repository/ColumnRepository.java
+++ src/main/java/com/example/demo/column/repository/ColumnRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.column.repository;
 
 import com.example.demo.column.entity.BoardColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -13,4 +14,8 @@ public interface ColumnRepository extends JpaRepository<BoardColumn, Long> {
     // 향후 사용가능성을 보고 확장 OR 삭제
     Optional<BoardColumn> findByName(String name);
     boolean existsByName(String name); // 이미 존재하는 '상태 이름' 을 확인하기 위한 메서드
+
+    // ColumnService 클래스에서 현재 최대 순서 값을 조회한 후 1을 더하는 방법을 사용하기 위해 메소드 추가
+    @Query("SELECT MAX(c.order) FROM BoardColumn c") // BoardColumn 엔티티의 order 속성 값 중 최대 값 선택
+    Optional<Long> findMaxOrder(); // 결과가 없을 수도 있기에 Optional을 사용하여 안전하게 처리
 }
