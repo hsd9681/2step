dbd3d62f2b9f29d5c1fbc619412f44445d42382c
gminnimk <tvt1245@naver.com>
2024-07-12 19:55:08 +0900

    fix: User 클래스 / User(1) : Permission(N) 관계에 대한 명확한 정의 (일대다 관계) , 권한을 반환하는 역할인 getPermission() 메서드 추가

--- src/main/java/com/example/demo/domain/user/entity/User.java
+++ src/main/java/com/example/demo/domain/user/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.user.entity;
 
 import com.example.demo.domain.permission.entity.Permission;
+import com.example.demo.domain.permission.entity.PermissionType;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,8 +27,8 @@ public class User {
     @Column
     private String refreshToken;
 
-    @Getter
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    // User(1) : Permission(N)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     private List<Permission> permissions = new ArrayList<>();
 
     @Builder
@@ -37,4 +38,20 @@ public class User {
         this.refreshToken = refreshToken;
     }
 
+    // 사용자의 권한을 반환하는 역할
+    public PermissionType getPermission() {
+        // permissions 가 null 이거나 리스타가 비어 있는 경우 USER 권한 반환
+        // null => 리스트가 초기화되지 않았거나 객체가 할당되지 않은 상태를 체크
+        // isEmpty() => permissions 리스트가 초기화되었지만 요소가 포함되지 않은 상태를 체크
+        if (permissions == null || permissions.isEmpty()) {
+            return PermissionType.USER; // 기본 권한
+        }
+
+        // MANAGER 권한이 있는지 확인
+        boolean hasManagerPermission = permissions.stream()
+                .anyMatch(permission -> PermissionType.MANAGER.equals(permission.getAuthority()));
+
+        // 사용자가 MANAGER 권한을 가질 시 MANAGER 반환, 아니면 USER 반환
+        return hasManagerPermission ? PermissionType.MANAGER : PermissionType.USER;
+    }
 }
