074113ba483f3015170e7a950e1bbe6952280e31
gminnimk <tvt1245@naver.com>
2024-07-12 14:23:36 +0900

    refactor: 기존 Dto 수정으로 ColumnController 코드 변경

--- src/main/java/com/example/demo/column/controller/ColumnController.java
+++ src/main/java/com/example/demo/column/controller/ColumnController.java
@@ -1,6 +1,7 @@
 package com.example.demo.column.controller;
 
-import com.example.demo.column.dto.ColumnDto;
+import com.example.demo.column.dto.RequestColumnDto;
+import com.example.demo.column.dto.ResponseColumnDto;
 import com.example.demo.column.service.ColumnService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +24,12 @@ public class ColumnController {
 
     // 컬럼 생성 (보드에 컬럼 생성)
     @PostMapping("/{id}/col")
-    public ResponseEntity<ColumnDto> createColumn(@Valid @RequestBody ColumnDto columnDto) {
+    public ResponseEntity<ResponseColumnDto> createColumn(@Valid @RequestBody RequestColumnDto requestColumnDto) {
         // TODO: 실제 인증 로직으로 대체해야 함
         // 현재는 권한이 항상 있다고 가정
         boolean hasPermission = true;
         // 컬럼을 생성하는 서비스 메서드 호출
-        ColumnDto createdColumn = columnService.createColumn(columnDto, hasPermission);
+        ResponseColumnDto createdColumn = columnService.createColumn(requestColumnDto, hasPermission);
         return new ResponseEntity<>(createdColumn, HttpStatus.CREATED);
     }
 
@@ -42,11 +43,11 @@ public class ColumnController {
     }
 
     @PutMapping("/reorder")
-    public ResponseEntity<List<ColumnDto>> reorderColumns(@RequestBody List<Long> columnIds) {
+    public ResponseEntity<List<ResponseColumnDto>> reorderColumns(@RequestBody List<Long> columnIds) {
         // TODO: 실제 인증 로직으로 대체해야 함
         // 현재는 권한이 항상 있다고 가정
         boolean hasPermission = true;
-        List<ColumnDto> reorderedColumns = columnService.reorderColumns(columnIds, hasPermission);
+        List<ResponseColumnDto> reorderedColumns = columnService.reorderColumns(columnIds, hasPermission);
         return new ResponseEntity<>(reorderedColumns, HttpStatus.OK);
     }
 }
