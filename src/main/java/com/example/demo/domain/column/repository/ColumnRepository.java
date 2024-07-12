1178773bac6204a606dcb039ae0aafe72b8e6470
gminnimk <tvt1245@naver.com>
2024-07-12 17:40:53 +0900

    fix: findByName 메서드 삭제

--- src/main/java/com/example/demo/domain/column/repository/ColumnRepository.java
+++ src/main/java/com/example/demo/domain/column/repository/ColumnRepository.java
@@ -10,9 +10,7 @@ import java.util.Optional;
 // JPA로 DB 에 접근하기 위한 ColumnRepository 인터페이스
 // BoardColumn 엔티티에 대한 DB 엑세스, CRUD, 커스텀 쿼리(EX.findByName) 지원
 public interface ColumnRepository extends JpaRepository<BoardColumn, Long> {
-    // 특정 컬럼 '상태 이름'으로 찾기 (현재는 사용 X, 조건에 특정 컬럼 조회 X)
-    // 향후 사용가능성을 보고 확장 OR 삭제
-    Optional<BoardColumn> findByName(String name);
+
     boolean existsByName(String name); // 이미 존재하는 '상태 이름' 을 확인하기 위한 메서드
 
     // ColumnService 클래스에서 현재 최대 순서 값을 조회한 후 1을 더하는 방법을 사용하기 위해 메소드 추가
